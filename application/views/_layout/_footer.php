21f28a502c9dd277a55ad7016b9a0a6bffa0966c
I Komang Sugiartha <sugiartha26@gmail.com>
2020-11-18 22:21:24 +0700

    Input Data Pegawai

---
+++ application/views/_layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+		Dashboard Admin
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2020 <a href="https://komangsugiartha.com">Komang Sugiartha</a>.</strong> All rights reserved.
+</footer>
